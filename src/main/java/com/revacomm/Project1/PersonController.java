3c7db11806a3b166decf74fc563381c35eb16f89
McCoy Doherty <mccoy@rc-mccoy-mbp.lan>
2022-08-29 12:31:23 -0500

    Added Retrieve/Delete by ID, added retreive by last-name, added db destroy and rebuild at /db

--- src/main/java/com/revacomm/Project1/PersonController.java
+++ src/main/java/com/revacomm/Project1/PersonController.java
@@ -18,8 +18,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 /*
-    https://www.codejava.net/frameworks/spring/understand-spring-data-jpa-with-simple-example
-    https://www.codejava.net/frameworks/spring-boot/connect-to-postgresql-database-examples
+    To-Do List
+        - Migrate the repository calls controller --> service
+        - Implement the POST route to create resources
+        - Implement to PUT route to update resources
  */
 
 @RestController
@@ -29,12 +31,71 @@ public class PersonController {
 
     private final AtomicLong counter = new AtomicLong();
 
+    // Returns all records
     @GetMapping("/person")
     public Object getAllEmployees() {
         return personRepository.findAll();
     }
 
-    @GetMapping("/tryGetID/{LastName}")
+    // Returns all records matching ID parameter from path
+    @GetMapping("/person/{id}")
+    public Object getPerson(@PathVariable int id){
+        return personRepository.findById(id);
+    }
+
+    // TO DO
+    // Should this be at a specific ID or just at /person then the ID gets assigned
+    @PutMapping("/person/{id}")
+    public Person updatePerson(@PathVariable int id){
+        return new Person(id, "Tester", "Who Is Updated", 25);
+    }
+
+    // TO DO
+    @PostMapping("/person/{id}")
+    public Person createPerson(@PathVariable int id){
+        return new Person(id, "Tester", "Who Is New", 25);
+    }
+
+    // Swapped to void to avoid errors but is that kosher or should there be some sort of "receipt of deletion"
+    @DeleteMapping("/person/{id}")
+    public void deletePerson(@PathVariable int id){
+        // could also be .deleteAllById()
+        personRepository.deleteById(id);
+    }
+
+    @PostMapping("/db")
+    public void populateDB(){
+        Person MattNath = new Person(1337, "Matt", "Nathanson", 49);
+        personRepository.save(MattNath);
+
+        Person America = new Person(1776, "John", "America", 245);
+        personRepository.save(America);
+
+        Person Sandler = new Person(2356, "Adam", "Sandler", 55);
+        personRepository.save(Sandler);
+
+        Person Bowie = new Person(4667, "David", "Bowie", 69);
+        personRepository.save(Bowie);
+
+        Person ChuckE = new Person(9898, "Chuck E.", "Cheese", 45);
+        personRepository.save(ChuckE);
+
+        Person Mozzarella = new Person(7366, "Mozzarella E.", "Cheese", 22);
+        personRepository.save(Mozzarella);
+
+        Person Appleseed = new Person(2039, "Johnny", "Appleseed", 17);
+        personRepository.save(Appleseed);
+    }
+
+    @DeleteMapping("/db")
+    public void depopulateDB(){
+        personRepository.deleteAll();
+    }
+
+    // ... Beyond Scope Code Below ... Retained for Reference ...
+
+    // Get records by last name (not required by spec, just wanted to try writing an interface + use String parameter
+    @GetMapping("/personByLastName/{LastName}")
     public Object tryGetID(@PathVariable String LastName) {
         return personRepository.findByLastName(LastName);
     }
@@ -43,27 +104,6 @@ public class PersonController {
     // This request-param annotation
     public Person getPerson(@RequestParam(value="firstName", defaultValue="defaultFirst") String firstName) {
         return new Person(1, firstName, "Doherty", 25);
-    }
-
-    @GetMapping("/person/{id}")
-    public Person getPerson(@PathVariable int id){
-        return new Person(id, "Tester", "McTestersen", 25);
-    }
-
-    // Should this be at a specific ID or just at /person then the ID gets assigned
-    @PutMapping("/person/{id}")
-    public Person updatePerson(@PathVariable int id){
-        return new Person(id, "Tester", "Who Is Updated", 25);
-    }
-
-    @PostMapping("/person/{id}")
-    public Person createPerson(@PathVariable int id){
-        return new Person(id, "Tester", "Who Is New", 25);
-    }
-
-    @DeleteMapping("/person/{id}")
-    public Person deletePerson(@PathVariable int id){
-        return new Person(id, "Deleted", "Deleterson", 0);
     }
 }
 
